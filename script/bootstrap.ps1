9803bb5234cf528b016f552fa29824bc00cebc6b
Martin Jul <martin@mjul.com>
2013-03-02 18:48:44 +0100

    PowerShell bootstrap script slightly improved
    Tests for access to Java SDK, better reporting.

--- script/bootstrap.ps1
+++ script/bootstrap.ps1
@@ -1,114 +1,124 @@
-
-
-$jarCommands = Get-ChildItem 'C:\Program Files\Java' -Include "jar.exe" -Recurse
-$jarCommand = $jarCommands[0]
-
-if (-not (test-path $jarCommand))
-{
-    Write-Error "Could not find Java SDK jar.exe command"
-    exit
-}
-else
-{
-    Write-Host "Using jar.exe from" $jarCommand
+# Check that the Java SDK "jar" tool is available
+$jarCommand = Get-Command "[j]ar.exe"
+if ((-not $jarCommand) -or -not (Test-Path $jarCommand)) 
+{ 
+    throw "could not find jar.exe, you need to install the Java SDK or add it to your PATH." 
 }
 
 
-# Function from http://serverfault.com/questions/313015/trying-to-unzip-a-file-with-powershell
+
+function Get-Absolute-Path
+{
+    param([string] $file)
+    if ([IO.Path]::IsPathRooted($file))
+    {
+        return $file
+    }
+    return ([IO.FileInfo] (Join-Path (Get-Location) $file)).FullName
+}
+
+
+
+
+# Function inspired by http://serverfault.com/questions/313015/trying-to-unzip-a-file-with-powershell
 function Extract-Zip 
 { 
-    param([string]$zipfilename, [string] $destination) 
-    if(test-path($zipfilename)) 
+    param([string]$ZipFile, [string] $Target) 
+    if(test-path($ZipFile)) 
     { 
+        $absZip = Get-Absolute-Path $ZipFile
+        $absTarget = Get-Absolute-Path $Target
+        Write-Host ("Unzipping {0} to {1}..." -f $absZip, $absTarget)
         $shellApplication = new-object -com shell.application 
-        $zipPackage = $shellApplication.NameSpace($zipfilename) 
-        $destinationFolder = $shellApplication.NameSpace($destination) 
+        $zipPackage = $shellApplication.NameSpace( $absZip )
+        $destinationFolder = $shellApplication.NameSpace( $absTarget )
         $destinationFolder.CopyHere($zipPackage.Items()) 
     }
     else 
     {   
-        Write-Error "Zip file:" $zipfilename "not found"
+        throw ("Zip file: {0} not found" -f $ZipFile)
     }
 } 
 
 function New-Directory
 {
-    param([string]$dir)
-    if (-not (test-path $dir))
+    param([string]$Dir)
+    if (-not (test-path $Dir))
     {
-        New-Item $dir -ItemType Directory
+        New-Item $Dir -ItemType Directory
     }
-    return (Get-Item $dir)
+    return (Get-Item $Dir)
 }
+
+function Download-Unzip
+{
+    param([string] $Uri, [string] $ZipFile, [string] $Target)
+    Write-Host ("Downloading {0}..." -f $Uri)
+    Invoke-RestMethod -Uri $Uri -OutFile $ZipFile
+    Extract-Zip -ZipFile $ZipFile -Target $Target
+}
+
+$libDir = Join-Path (Get-Location) "lib"
+New-Directory $libDir
+
+# --------------------------------------------------------------
 
 $clojureZip = Join-Path (Get-Location) "clojure-1.4.0.zip"
 $clojureDir = Join-Path (Get-Location) "clojure-1.4.0"
-$libDir = Join-Path (Get-Location) "lib"
 
-New-Directory $libDir
-New-Directory $clojureDir
-$targetDir = get-item $clojureDir 
-
-echo "Fetching Clojure..."
-Invoke-RestMethod -Uri  http://repo1.maven.org/maven2/org/clojure/clojure/1.4.0/clojure-1.4.0.zip -OutFile $clojureZip
-Extract-Zip $clojureZip (Get-Location)
-
-echo "Copying clojure-1.4.0/clojure-1.4.0.jar to lib/clojure.jar..."
-Copy-Item $clojureDir/clojure-1.4.0.jar $libDir/clojure.jar
-
+Write-Host "Fetching Clojure..." -ForegroundColor Cyan
+Download-Unzip -Uri http://repo1.maven.org/maven2/org/clojure/clojure/1.4.0/clojure-1.4.0.zip -ZipFile $clojureZip -Target .
+Copy-Item .\clojure-1.4.0\clojure-1.4.0.jar (Join-Path $libDir clojure.jar)
 echo "Cleaning up Clojure directory..."
 Remove-Item -Recurse $clojureDir
-
 echo "Cleaning up Clojure archive..."
 Remove-Item -Recurse $clojureZip
 
+# --------------------------------------------------------------
 
-echo "Fetching Google Closure library..."
+Write-Host "Fetching Google Closure library..." -ForegroundColor Cyan
+
 $closureLibrary = New-Directory "closure/library"
 $closure = "closure-library-20120710-r2029.zip"
 $closureUri = "http://closure-library.googlecode.com/files/" + $closure
 $closureZip = Join-Path $closureLibrary $closure 
 
-Invoke-RestMethod -Uri $closureUri -OutFile $closureZip
-Extract-Zip $closureZip $closureLibrary
+Download-Unzip -Uri $closureUri -ZipFile $closureZip -Target $closureLibrary
 
-echo "Cleaning up Google Closure library archive..."
+Write-Host "Cleaning up Google Closure library archive..."
 Remove-Item $closureZip
 
+# --------------------------------------------------------------
 
-
-echo "Fetching Google Closure compiler..."
+Write-Host "Fetching Google Closure compiler..." -ForegroundColor Cyan
 $compilerDirectory = New-Directory "compiler"
 $compilerUri = "http://closure-compiler.googlecode.com/files/compiler-latest.zip"
 $compilerZip = Join-Path $compilerDirectory "compiler-latest.zip"
+$compilerJar = Join-Path $compilerDirectory "compiler.jar"
+
+Download-Unzip -Uri $compilerUri -ZipFile $compilerZip -Target $compilerDirectory
 Invoke-RestMethod -Uri $compilerUri -OutFile $compilerZip
 Extract-Zip $compilerZip $compilerDirectory
 
-echo "Cleaning up Google Closure compiler archive..."
-rm $compilerZip
+Write-Host "Cleaning up Google Closure compiler archive..."
+Remove-Item $compilerZip
 
-$compilerJar = Join-Path $compilerDirectory "compiler.jar"
+Write-Host "Building lib/goog.jar..."
+& $jarCommand cf (Join-Path $libDir goog.jar) -C (Join-Path $closureLibrary closure) goog
 
+Copy-Item $compilerJar $libDir
 
-echo "Building lib/goog.jar..."
-echo "jar cf ./lib/goog.jar -C closure/library/closure/ goog"
-& $jarCommand cf ./lib/goog.jar -C closure/library/closure/ goog
+# --------------------------------------------------------------
 
-echo "Fetching Rhino..."
+Write-Host "Fetching Rhino..." -ForegroundColor Cyan
 $rhinoUri = "http://ftp.mozilla.org/pub/mozilla.org/js/rhino1_7R3.zip"
 $rhinoZip =  Join-Path (Get-Location) "rhino1_7R3.zip"
-Invoke-RestMethod -Uri $rhinoUri -OutFile $rhinoZip
-Extract-Zip $rhinoZip (Get-Location)
-
-
-echo "Copying rhino1_7R3/js.jar to lib/js.jar..."
-cp rhino1_7R3/js.jar lib/js.jar
-echo "Cleaning up Rhino directory..."
-Remove-Item -Recurse rhino1_7R3
-echo "Cleaning up Rhino archive..."
+$rhinoDir =  Join-Path (Get-Location) rhino1_7R3
+Download-Unzip -Uri $rhinoUri -ZipFile $rhinoZip -Target .
+Copy-Item (Join-Path $rhinoDir js.jar) (Join-Path $libDir js.jar)
+Remove-Item -Recurse $rhinoDir
 Remove-Item $rhinoZip
 
-echo "Copying closure/compiler/compiler.jar to lib/compiler.jar"
-Copy-Item $compilerJar lib
+# --------------------------------------------------------------
 
-echo "[Bootstrap Completed]"
+Write-Host "[Bootstrap Completed]"
